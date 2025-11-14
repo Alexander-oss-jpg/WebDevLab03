38c8001a626e7de8b8ec6bb529a8906ac570aae5
Alexander-oss-jpg <Alexanderj3204@gmail.com>
2025-11-14 15:48:44 -0500

    Create 2_Alexander_Jaber_Portfolio.py

---
+++ pages/2_Alexander_Jaber_Portfolio.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("Alexander Jaber Portfolio")
+
+st.header("About Me")
+st.write("""
+Hello! I'm Alex and this is my portfolio page showcasing my skills and projects.
+""")
+
+st.subheader("🎓 Education")
+st.write("""
+- **Georgia Institute of Technology**
+- Major: [Industrial Engineering]
+- Expected Graduation: [2028]
+""")
+st.subheader("TENNIS LOVER")
+
+st.subheader("📬 Contact Information")
+st.write("- Email: ajaber6@gatech.edu")
+st.write("- Add_my_IG_IF YOU WANT :ajab_0321")
+
+st.divider()
+st.info("Thank you for visiting my portfolio!")
