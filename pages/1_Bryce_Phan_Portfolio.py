31120268e23eb33b54e83e97d59a147243e5dfd7
Alexander-oss-jpg <Alexanderj3204@gmail.com>
2025-11-14 15:48:17 -0500

    Create 1_Bryce_Phan_Portfolio.py

---
+++ pages/1_Bryce_Phan_Portfolio.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("Bryce Phan Description")
+
+st.header("About Me")
+st.write("""
+Hello! I'm Bryce Phan, and this is my description page showing who I am and my projects.
+""")
+
+st.subheader("Education")
+st.write("""
+- **Georgia Institute of Technology**
+- Major: Industrial Engineer
+- Expected Graduation: 2028
+""")
+
+st.write("Tennis enjoyer!")
+
+
+
+st.subheader("Contact Me!")
+st.write("- Email: ybphan36@gatech.edu")
+st.write("- IG: phan.bryce")
+
+
+st.divider()
+st.info("Thank you for visiting my portfolio!")
